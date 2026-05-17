b9115bdc64225dbcb3752ac2d8217fe4473de30f
Javokhirbek Khaydaraliev <khaydaraliev99@gmail.com>
2026-05-17 19:14:40 +0500

    Added Feed Forward Layer which brought down the loss to ~2.29

--- main.py
+++ main.py
@@ -89,55 +89,72 @@ class MultiHeadAttention(nn.Module):
        return torch.cat([h(x) for h in self.heads], dim=-1)
 
 
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, n_embed),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+       return self.net(x)
+
 
 class BigramLanguageModel(nn.Module):
 
-  def __init__(self):
-    super().__init__()
-    # each token directly reads off the logits for the next token from a lookup table
-    self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
-    self.position_embedding_table = nn.Embedding(block_size, n_embed)
-    self.sa_heads = MultiHeadAttention(4, n_embed//4) # i.e. 4 heads of 8-dimensional self-attention
-    self.lm_head = nn.Linear(n_embed, vocab_size)
-  
-  def forward(self, idx, targets=None):
-    B, T = idx.shape
 
+    def __init__(self):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.sa_heads = MultiHeadAttention(4, n_embed//4) # i.e. 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embed)
+        self.lm_head = nn.Linear(n_embed, vocab_size)
     
-    # idx and targets are both (B,T) tensor of integers
-    token_emb = self.token_embedding_table(idx) # (B,T,C)
-    pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
-    x = token_emb + pos_emb # (B,T,C)
-    x = self.sa_heads(x) # apply one head of self-attention. (B, T, C)
-    logits = self.lm_head(x) # # (B,T,vocab_size)
+    def forward(self, idx, targets=None):
+        
+        B, T = idx.shape
 
-    if targets is None:
-      loss = None
-    else:
-      B, T, C = logits.shape
-      logits = logits.view(B*T, C)
-      targets = targets.view(B*T)
-      loss = F.cross_entropy(logits, targets)
+        
+        # idx and targets are both (B,T) tensor of integers
+        token_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = token_emb + pos_emb # (B,T,C)
+        x = self.sa_heads(x) # apply one head of self-attention. (B, T, C)
+        x = self.ffwd(x) # (B,T,C)
+        logits = self.lm_head(x) # # (B,T,vocab_size)
 
-    return logits, loss
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
 
-  def generate(self, idx, max_new_tokens):
-    # idx is (B, T) array of indices in the current context
-    for _ in range(max_new_tokens):
-        # crop idx to the last block_size tokens
-        idx_cond = idx[:, -block_size:]
-        # get the predictions
-        logits, loss = self(idx_cond)
-        # focus only on the last time step
-        logits = logits[:, -1, :] # becomes (B, C)
-        # apply softmax to get probabilities
-        probs = F.softmax(logits, dim=-1) # (B, C)
-        # sample from the distribution
-        idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-        # append sampled index to the running sequence
-        idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
-    
-    return idx
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        
+        return idx
   
 
 
